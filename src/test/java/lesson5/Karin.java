1a1ca93dd36654d1f7d715cb17e7f2785e0a1df8
SgnvJaber <sgnvunleashed5@gmail.com>
2021-10-31 14:35:02 +0200

    working with maven

---
+++ src/test/java/lesson5/Karin.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+import org.testng.annotations.Test;
+
+public class Karin {
+
+    @Test
+    public void test01()
+    {
+        System.out.println("Hello Karin 1");
+    }
+    @Test
+    public void test02()
+    {
+        System.out.println("Hello Karin 2");
+    }
+}
